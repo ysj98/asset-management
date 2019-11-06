68606b24c10b0b767aaa5f109611ad0926ea8b80
liwang <liwang@segimail.com>
2019-11-06 11:14:06 +0800

    资产变动添加api

--- src/config/config.url.js
+++ src/config/config.url.js
@@ -38,5 +38,30 @@ export const assets = {
     getProject: '/getProject',
     // 异步组织机构树 组织机构管理 POST
     queryAsynOrganByUserId: '/basic-data-api/rest-api/v1/organApi/queryAsynOrganByUserId'
+  },
+  // 资产变动
+  management: {
+    // 资产变动-一览表-分页列表
+    getChangeSchedulePage: '/ams/assetCleanup/getChangeSchedulePage',
+    // 资产变动-详情
+    getChangeDetail: '/ams/assetCleanup/getChangeDetail',
+    // 资产变动-一览表-导出
+    exportChangeScheduleList: '/ams/assetCleanup/exportChangeScheduleList',
+    // 资产变动-分页列表
+    getChangePage: '/ams/assetCleanup/getChangePage',
+    // 资产变动-删除
+    deleteChange: '/ams/assetChange/deleteChange',
+    // 资产变动-审批
+    approveChange: '/ams/assetChange/approveChange',
+    // 资产变动-导入
+    importChange: '/ams/assetChange/importChange',
+    // 资产变动-导出
+    exportChange: '/ams/assetChange/exportChange',
+    // 资产变动-新增编辑
+    updateInsertChange: '/ams/assetCleanup/updateInsertChange',
+    // 资产变动-明细
+    getChangeInfo: '/ams/assetCleanup/getChangeInfo',
+    // 资产变动-终止交付
+    stopDelivery: '/ams/assetChange/stopDelivery'
   }
 }
--- src/views/assetInformationManagement/assetChange/scheduleChanges.vue
+++ src/views/assetInformationManagement/assetChange/scheduleChanges.vue
@@ -3,14 +3,33 @@
 -->
 <template>
   <div class="assetsRegistration">
-    <Cephalosome>
-      <div slot="col-l">
-        <a-button icon="plus" type="primary">新建变动单</a-button>
+    <SearchContainer v-model="toggle" @input="searchContainerFn">
+      <div slot="headerBtns">
+        <SG-Button type="primary"><segiIcon type="#icon-ziyuan10" class="icon-right"/>导出</SG-Button>
       </div>
-      <div slot="col-r">
-        <treeSelect @changeTree="changeTree"  placeholder='请选择组织机构' :allowClear="false" style="width: 150px"></treeSelect>
+      <div slot="headerForm">
+        <treeSelect @changeTree="changeTree"  placeholder='请选择组织机构' :allowClear="false" :style="allStyle"></treeSelect>
+        <a-input-search placeholder="资产名称/编码" style="width: 140px; margin-right: 10px;" @search="onSearch" />
       </div>
-    </Cephalosome>
+      <div slot="contentForm">
+        <div>
+          <a-select
+            :maxTagCount="1"
+            :style="allStyle"
+            mode="multiple"
+            :tokenSeparators="[',']"
+            placeholder="全部状态"
+            :defaultValue="queryCondition.approvalStatus"
+            style="width: 200px"
+          >
+            <a-select-option v-for="(item, index) in approvalStatusData" :key="index" :value="item.value">{{item.name}}</a-select-option>
+          </a-select>
+        </div>
+        <div>
+          234
+        </div>
+      </div>
+    </SearchContainer>
     <div>
       <a-table
         :columns="columns"
@@ -27,9 +46,11 @@
 </template>
 
 <script>
-import Cephalosome from '@/components/Cephalosome'
+// import Cephalosome from '@/components/Cephalosome'
+import SearchContainer from '@/views/common/SearchContainer'
 import TreeSelect from '../../common/treeSelect'
 import OperationPopover from '@/components/OperationPopover'
+import segiIcon from '@/components/segiIcon.vue'
 const columns = [
   {
     title: 'name',
@@ -56,11 +77,39 @@ const operationData = [
   {iconType: 'form', text: '修改', editType: 'edit'},
   {iconType: 'delete', text: '删除', editType: 'delete'}
 ]
+const approvalStatusData = [
+  {
+    name: '全部状态',
+    value: ''
+  },
+  {
+    name: '草稿',
+    value: '0'
+  },
+  {
+    name: '待审批',
+    value: '2'
+  },
+  {
+    name: '已驳回',
+    value: '3'
+  },
+  {
+    name: '已审批',
+    value: '1'
+  },
+  {
+    name: '已取消',
+    value: '4'
+  }
+]
 export default {
-  components: {Cephalosome, TreeSelect, OperationPopover},
+  components: {SearchContainer, TreeSelect, OperationPopover, segiIcon},
   props: {},
   data () {
     return {
+      allStyle: 'width: 170px; margin-right: 10px;',
+      toggle: true,
       columns,
       organName: '',
       organId: '',
@@ -72,7 +121,11 @@ export default {
           address: 'address'
         }
       ],
-      operationData: [...operationData]
+      operationData: [...operationData],
+      approvalStatusData: [...approvalStatusData],
+      queryCondition: {
+        approvalStatus: ['']
+      }
     }
   },
   computed: {
@@ -82,7 +135,14 @@ export default {
       this.organName = label
       this.organId = value
     },
-    operationFun () {}
+    // 详情
+    operationFun () {},
+    // 搜索
+    onSearch () {},
+    // 高级搜索控制
+    searchContainerFn (val) {
+      this.toggle = val
+    }
   },
   created () {
   },
