84a841809dc1ffd8a5958366590ab91b23d75775
Young <1who@sina.cn>
2019-11-30 19:37:51 +0800

    修改价值登记逻辑

--- src/views/financialManagement/worth-register/asset-worth-register/components/BaseInfoPart.vue
+++ src/views/financialManagement/worth-register/asset-worth-register/components/BaseInfoPart.vue
@@ -1,12 +1,129 @@
+<!--价值登记页面--资产价值登记详情页面--基础信息组件-->
 <template>
-  <div></div>
+  <div class="base_info_form">
+    <SG-Title title="基础信息"/>
+    <a-form :form="form" layout="horizontal">
+    <a-row :gutter="24">
+      <a-col :span="8">
+        <a-form-item label="所属组织机构" :label-col="formItemLayout.labelCol" :wrapper-col="formItemLayout.wrapperCol">
+          <a-input v-decorator="[ 'organName']" disabled/>
+        </a-form-item>
+      </a-col>
+      <a-col :span="8">
+        <a-form-item :label-col="formItemLayout.labelCol" :wrapper-col="formItemLayout.wrapperCol" label="价值登记名称">
+          <a-input
+            placeholder="请填写价值登记名称"
+            v-decorator="[ 'registerName', { rules: [{ required: true, message: '请填写价值登记名称' }, {max: 30, message: '最多30个字符'}] } ]"
+          />
+        </a-form-item>
+      </a-col>
+      <a-col :span="8">
+        <a-form-item :label-col="formItemLayout.labelCol" :wrapper-col="formItemLayout.wrapperCol" label="所属评估任务">
+          <a-select
+            v-decorator="['taskId', { rules: [{ required: true, message: '请选择所属评估任务' }] }]"
+            placeholder="请选择所属评估任务"
+            :options="taskOptions"
+          />
+        </a-form-item>
+      </a-col>
+    </a-row>
+    <a-row :gutter="24">
+      <a-col :span="8">
+        <a-form-item :label-col="formItemLayout.labelCol" :wrapper-col="formItemLayout.wrapperCol" label="资产项目">
+          <a-select
+            v-decorator="['projectId', { rules: [{ required: true, message: '请选择资产项目' }] }]"
+            placeholder="请选择资产项目"
+            :options="projectOptions"
+          />
+        </a-form-item>
+      </a-col>
+      <a-col :span="8">
+        <a-form-item :label-col="formItemLayout.labelCol" :wrapper-col="formItemLayout.wrapperCol" label="资产类型">
+          <a-select
+            v-decorator="['typeId', { rules: [{ required: true, message: '请选择资产类型' }] }]"
+            placeholder="请选择资产类型"
+            :options="typeOptions"
+          />
+        </a-form-item>
+      </a-col>
+      <a-col :span="8">
+        <a-form-item label="评估基准日" :label-col="formItemLayout.labelCol" :wrapper-col="formItemLayout.wrapperCol">
+          <a-date-picker
+            :disabled="isEdit"
+            style="width: 100%"
+            placeholder="请选择评估基准日"
+            v-decorator="['transferApprovalDate', { rules: [{ required: true, message: '请选择评估基准日' }] }]"
+          />
+        </a-form-item>
+      </a-col>
+    </a-row>
+    <a-row :gutter="24">
+      <a-col :span="8">
+        <a-form-item :label-col="formItemLayout.labelCol" :wrapper-col="formItemLayout.wrapperCol" label="评估方法">
+          <a-select
+            v-decorator="['typeId', { rules: [{ required: true, message: '请选择评估方法' }] }]"
+            placeholder="请选择评估方法"
+            :options="methodOptions"
+          />
+        </a-form-item>
+      </a-col>
+      <a-col :span="8">
+        <a-form-item :label-col="formItemLayout.labelCol" :wrapper-col="formItemLayout.wrapperCol" label="评估机构">
+          <a-select
+            v-decorator="['organVal', { rules: [{ required: true, message: '请选择评估机构' }] }]"
+            placeholder="请选择评估机构"
+            :options="organOptions"
+          />
+        </a-form-item>
+      </a-col>
+    </a-row>
+    <a-row>
+      <a-col :span="24">
+        <a-form-item label="附件" :label-col="{span: 2}" :wrapper-col="{span: 21}">
+          <SG-UploadFile
+            :show="!isEdit"
+            v-if="isEdit || attachment.length"
+            v-model="attachment"
+          />
+          <span v-else style="margin-left: 12px">无</span>
+        </a-form-item>
+      </a-col>
+    </a-row>
+    <a-row>
+      <a-col :span="24">
+        <a-form-item label="备注" :label-col="{span: 2}" :wrapper-col="{span: 21}">
+          <a-textarea
+            :rows="4"
+            :disabled="isEdit"
+            style="resize: none"
+            placeholder="请输入备注"
+            v-decorator="['remark', { rules: [{max: 200, message: '最多200个字符'}] }]"
+          />
+        </a-form-item>
+      </a-col>
+    </a-row>
+  </a-form>
+  </div>
 </template>
 
 <script>
   export default {
     name: 'BaseInfoPart',
     data () {
-      return {}
+      return {
+        formItemLayout: {
+          labelCol: { span: 6 },
+          wrapperCol: { span: 18 }
+        },
+        attachment: [], // 附件
+        isEdit: false, // 是否编辑状态
+        typeOptions: [], // 资产类型选项,
+        taskOptions: [],  // 评估任务选项
+        organOptions: [], // 评估机构选项
+        form: this.$form.createForm(this),
+        methodOptions: [], // 评估方法选项
+        projectOptions: [], // 资产项目选项
+      }
     },
 
     methods: {}
@@ -14,5 +131,9 @@
 </script>
 
 <style lang='less' scoped>
-
+  .base_info_form {
+    /*& /deep/ .ant-form-item-label label{*/
+      /*text-align: justify;*/
+    /*}*/
+  }
 </style>
--- src/views/financialManagement/worth-register/asset-worth-register/components/WorthListPart.vue
+++ src/views/financialManagement/worth-register/asset-worth-register/components/WorthListPart.vue
@@ -1,18 +1,110 @@
+<!--价值登记页面--资产价值登记详情页面--资产价值清单组件-->
 <template>
-  <div></div>
+  <div class="worth_list">
+    <SG-Title title="资产价值清单"/>
+    <div style="margin-bottom: 16px; overflow: hidden">
+      <div style="float: right">
+        <SG-Button icon="import" type="primary" ghost @click="handleExport" :loading="exportBtnLoading" >批量导入</SG-Button>
+        <SG-Button icon="export" style="margin: 0 10px" type="primary" ghost @click="handleExport" :loading="exportBtnLoading" >批量导出</SG-Button>
+        <SG-Button icon="delete" type="primary" ghost @click="handleExport" :loading="exportBtnLoading" >删除</SG-Button>
+      </div>
+    </div>
+    <a-table
+      v-bind="tableObj"
+      :rowSelection="rowSelection"
+      class="custom-table td-pd10 table-border"
+    />
+    <div v-if="!tableObj.dataSource.length" style="text-align: center; margin-top: 25px">暂无数据</div>
+    <!-- 选择资产 -->
+    <AssetBundlePopover ref="assetModal" :organId="organId" queryType="2"></AssetBundlePopover>
+  </div>
 </template>
 
 <script>
+  import AssetBundlePopover from 'src/views/common/assetBundlePopover'
   export default {
     name: 'WorthListPart',
+    components: { AssetBundlePopover },
     data () {
-      return {}
+      return {
+        organId: '',
+        tableObj: {
+          dataSource: [],
+          loading: false,
+          pagination: false,
+          rowKey: 'projectId',
+          scroll: { x: true },
+          columns: [
+            { title: '编号', dataIndex: 'assetObjectId' },
+            { title: '资产名称', dataIndex: 'assetName' },
+            { title: '资产编码', dataIndex: 'assetCode' },
+            { title: '所属机构', dataIndex: 'organName' },
+            { title: '所属资产项目', dataIndex: 'projectName' },
+            { title: '资产类型', dataIndex: 'assetTypeName' },
+            { title: '资产分类', dataIndex: 'assetObjectTypeName' },
+            { title: '资源原值(元)', dataIndex: 'originalValue' },
+            { title: '评估原值(元)', dataIndex: 'assetValuation' },
+            { title: '原值评估基准日', dataIndex: 'originalAssessmenBaseDate' },
+            { title: '上次评估方法', dataIndex: 'lastAssessmentMethodName' },
+            { title: '上次评估值', dataIndex: 'lastAssessmentValue' },
+            { title: '本次评估方法', dataIndex: 'assessmentMethodName' },
+            { title: '本次估值(元)', dataIndex: 'assessmentValue' },
+            { title: '评估机构', dataIndex: 'assessmentOrganName' },
+            { title: '评估基准日', dataIndex: 'assessmenBaseDate' },
+            { title: '上浮比', dataIndex: 'upRate' },
+          ]
+        },
+        selectedRowKeys: [], // Table选中的值
+        exportBtnLoading: false, // 导出按钮loading
+      }
     },
 
-    methods: {}
+    methods: {
+      // 导出
+      handleExport () {
+        this.$refs.assetModal.show = true
+      },
+      
+      // 选中
+      selectChange () {}
+    },
+
+    computed: {
+      // Table rowSelection
+      rowSelection() {
+        const { selectedRowKeys } = this
+        return {
+          onChange: (selectedRowKeys, selectedRows) => {
+            console.log(`selectedRowKeys: ${selectedRowKeys}`, 'selectedRows: ', selectedRows);
+          },
+          getCheckboxProps: record => ({
+            props: {
+              // disabled: record.name === 'Disabled User', // Column configuration not to be checked
+              // name: record.name,
+              style: {
+                display: record.assetName === '合计' ? 'none' : 'block'
+              }
+            }
+          })
+        }
+      }
+    }
   }
 </script>
 
 <style lang='less' scoped>
-
+  .worth_list {
+    .custom-table {
+      /*if you want to set scroll: { x: true }*/
+      /*you need to add style .ant-table td { white-space: nowrap; }*/
+      & /deep/ .ant-table-thead th, .ant-table td {
+        white-space: nowrap;
+      }
+      & /deep/ .ant-table-body {
+        &::-webkit-scrollbar {
+          height: 8px !important;
+        }
+      }
+    }
+  }
 </style>
