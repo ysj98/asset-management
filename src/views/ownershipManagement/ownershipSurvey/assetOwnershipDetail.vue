d7699e884368ab57ab6629d4ce0c7dafd17cb1ee
陈瀚 <chenhan@segimail.com>
2019-12-16 17:15:01 +0800

    修改端

--- src/views/ownershipManagement/ownershipSurvey/assetOwnershipDetail.vue
+++ src/views/ownershipManagement/ownershipSurvey/assetOwnershipDetail.vue
@@ -267,7 +267,7 @@ const tableHandlerColumns = [
   },
   {
     title: "申请人",
-    dataIndex: "creatByName",
+    dataIndex: "createByName",
     width: "10%"
   },
   {
@@ -348,6 +348,7 @@ export default {
           });
           // 权属办理记录
           this.tableHandler.dataSource = transactionList.map(item => {
+            item.createByName = item.createByName || '--'
             return {
               key: getUuid(),
               ...item
