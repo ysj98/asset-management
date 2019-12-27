d04f1662390c7fd0982b7185b6ea6406e07b54e8
戚斯婷 <qist@segimail.com>
2019-12-27 11:38:15 +0800

    修改盘点管理api

--- src/api/inventoryManagementApi.js
+++ src/api/inventoryManagementApi.js
@@ -54,3 +54,32 @@ export function assetCheckInstDeleteByCheckResult (data) {
 export function assetCheckInstCheckExe (data) {
   return axiosPost(inventoryManagement.inventoryPerform.assetCheckInstCheckExe, data)
 }
+
+
+// 盘点管理 - 盘点报告
+// 新建或编辑盘点报告
+export function saveOrUpdate (data) {
+  return axiosPost(inventoryManagement.saveOrUpdate, data, false, false)
+}
+// 查询盘点报告列表(选择组织机构下的所有机构)
+export function checkReportList (data) {
+  return axiosPost(inventoryManagement.checkReportList, data, true, false)
+}
+// 查询盘点报告详情
+export function detail (data) {
+  return axiosGet(inventoryManagement.detail, data)
+}
+// 盘点报告删除
+export function deleteReport (data) {
+  return axiosPost(inventoryManagement.deleteReport, data)
+}
+
+// 盘点管理 - 盘点异常
+// 异常处理
+export function handleException (data) {
+  return axiosPost(inventoryManagement.handleException, data, false, false)
+}
+// 异常管理列表查询
+export function exceptionList (data) {
+  return axiosPost(inventoryManagement.exceptionList, data, true, false)
+}
