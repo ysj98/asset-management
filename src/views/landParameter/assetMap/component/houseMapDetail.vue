26297e91d1905385fd151bf591d186dfc25a14c1
陈瀚 <chenhan@segimail.com>
2020-09-09 11:20:44 +0800

    修改资产地图

---
+++ src/views/landParameter/assetMap/component/houseMapDetail.vue
@@ -0,0 +1,265 @@
+<!--
+ * @Date: 2020-09-08 14:54:39
+ * @Description: 地图房屋资产详情
+ * @Author: chh
+-->
+<template>
+  <div class="landMapDetail-page">
+    <div class="detail-top-head">
+      {{detailInfo.buildName | filterNullValue}}（楼栋）
+      <span class="fr pointer" @click="handleSwitch"
+        ><a-icon type="close"
+      /></span>
+    </div>
+    <div class="detail-page">
+      <!-- 详情部分 -->
+      <div class="detail-top">
+        <div class="detail-top-content">
+          <div class="detail-content-left">
+            <a-row>
+              <a-col :span="12">
+                <div class="detail-top-item">
+                  <span class="detail-label">丘地号：</span
+                  ><span class="detail-main">{{detailInfo.addressNo | filterNullValue}}</span>
+                </div>
+              </a-col>
+              <a-col :span="12">
+                <div class="detail-top-item">
+                  <span class="detail-label">总层数：</span
+                  ><span class="detail-main">{{detailInfo.buildFloor | filterNullValueSquare}}</span>
+                </div>
+              </a-col>
+              <a-col :span="12">
+                <div class="detail-top-item">
+                  <span class="detail-label">房屋数量：</span
+                  ><span class="detail-main">{{detailInfo.houseNum | filterNullValue}}</span>
+                </div>
+              </a-col>
+              <a-col :span="12">
+                <div class="detail-top-item">
+                  <span class="detail-label">建筑面积：</span
+                  ><span class="detail-main">{{detailInfo.builtArea | filterNullValue}}</span>
+                </div>
+              </a-col>
+              <a-col :span="12">
+                <div class="detail-top-item">
+                  <span class="detail-label">楼栋类型：</span
+                  ><span class="detail-main">{{detailInfo.buildType | filterNullValue}}</span>
+                </div>
+              </a-col>
+              <a-col :span="12">
+                <div class="detail-top-item">
+                  <span class="detail-label">用途：</span
+                  ><span class="detail-main">{{detailInfo.useType | filterNullValue}}</span>
+                </div>
+              </a-col>
+              <a-col :span="12">
+                <div class="detail-top-item">
+                  <span class="detail-label">建筑年代：</span
+                  ><span class="detail-main">{{detailInfo.builtAge | filterNullValueSquare}}</span>
+                </div>
+              </a-col>
+              <a-col :span="12">
+                <div class="detail-top-item">
+                  <span class="detail-label">建筑结构：</span
+                  ><span class="detail-main">{{detailInfo.buildStruct | filterNullValue}}</span>
+                </div>
+              </a-col>
+              <a-col :span="24">
+                <div class="detail-top-item">
+                  <span class="detail-label">地理位置：</span
+                  ><span class="detail-main"
+                    >{{detailInfo.position | filterNullValue}}
+                  </span>
+                </div>
+              </a-col>
+            </a-row>
+          </div>
+          <div class="detail-content-right"></div>
+        </div>
+      </div>
+      <!-- 表格部分 -->
+      <div class="detail-table">
+        <div class="pb10 pt10">
+          <SG-Title title="资产概况" noMargin />
+        </div>
+        <a-table class="table-boxs" bordered v-bind="table"> </a-table>
+        <div class="pb10 pt10">
+          <SG-Title title="权属概况" noMargin />
+        </div>
+        <a-table class="table-boxs" bordered v-bind="tableTwo"> </a-table>
+        <div class="pb10 pt10">
+          <SG-Title title="使用方向" noMargin />
+        </div>
+        <a-table class="table-boxs" bordered v-bind="tableThree"> </a-table>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Tools from '@/utils/utils'
+const columns = [
+  {
+    title: "资产数量(个)",
+    dataIndex: "assetNum",
+  },
+  {
+    title: "资产面积(㎡)",
+    dataIndex: "assetArea",
+  },
+   {
+    title: "资产价值(万元)",
+    dataIndex: "assetValue",
+  },
+]
+
+const columnsTwo = [
+  {
+    title: "所有权",
+    dataIndex: "ownerShipPercent",
+  },
+  {
+    title: "使用权",
+    dataIndex: "onlyUsePercent",
+  },
+  {
+    title: "有证",
+    dataIndex: "warrantPercent",
+  },
+  {
+    title: "无证",
+    dataIndex: "noWarrantPercent",
+  }
+]
+const columnsThree = [
+  {
+    title: "运营",
+    dataIndex: "transferOperationAreaPercent",
+  },
+  {
+    title: "自用",
+    dataIndex: "selfUserAreaPercent",
+  },
+  {
+    title: "闲置",
+    dataIndex: "idleAreaPercent",
+  },
+  {
+    title: "占用",
+    dataIndex: "occupationAreaPercent",
+  },
+  {
+    title: "其他",
+    dataIndex: "otherAreaPercent",
+  },
+]
+let getDataRow = (obj, columns) => {
+  let keys = columns.map(item => item.dataIndex)
+  let o = {key: Tools.getUuid()}
+  keys.forEach(item => {
+    o[item] = obj[item]
+  })
+  return o
+}
+export default {
+  name: "landMapDetail",
+  props: {
+    detailInfo: {
+      type: Object,
+      default: () => ({})
+    }
+  },
+  data() {
+    return {
+      // 表格数据
+      table: {
+        columns,
+        dataSource: [],
+        pagination: false,
+      },
+      // 表格2
+      tableTwo: {
+        columns: columnsTwo,
+        dataSource: [],
+        pagination: false,
+      },
+      // 表格3
+      tableThree: {
+        columns: columnsTwo,
+        dataSource: [],
+        pagination: false,
+      },
+    }
+  },
+  watch: {
+    detailInfo (nv) {
+      if (nv) {
+        this.table.dataSource = [getDataRow(nv, columns)]
+        this.tableTwo.dataSource = [getDataRow(nv, columnsTwo)]
+        this.tableThree.dataSource = [getDataRow(nv, columnsThree)]
+        console.log('拿到数据', getDataRow(nv, columns), getDataRow(nv, columnsTwo))
+      }
+    }
+  },
+  filters: {
+    filterNullValue (val) {
+      return val ? val : '-'
+    },
+    filterNullValueSquare (val) {
+      return val ? (val + '㎡') : '-'
+    },
+    filterNullValueDateRange (detailInfo) {
+      if (detailInfo.startDate) {
+       return detailInfo.startDate + '~' + detailInfo.endDate
+      }
+      return '-'
+    }
+  },
+  methods: {
+    handleSwitch() {
+      this.$emit("close", "land")
+    },
+  },
+}
+</script>
+
+<style lang="less" scoped>
+.landMapDetail-page {
+  width: 450px;
+  height: auto;
+  background: #ffffff;
+  box-shadow: 1px 2px 2px 0px rgba(0, 0, 0, 0.14);
+  border-radius: 4px;
+  font-size: 13px;
+  overflow: hidden;
+}
+.detail-page {
+  max-height: calc(100vh - 96px);
+  overflow-y: auto;
+}
+.detail-table {
+  padding: 0 15px 15px 15px;
+}
+.detail-top-head {
+  padding: 0 15px;
+  height: 32px;
+  line-height: 32px;
+  border-bottom: 1px solid #fff;
+  background-color: #0084ff;
+  color: #fff;
+}
+.detail-top {
+  background-color: #0084ff;
+  color: #fff;
+  padding-bottom: 15px;
+  .detail-top-content {
+    display: grid;
+    grid-template-columns: auto 124px;
+    margin: 0 15px;
+  }
+  .detail-top-item {
+    margin-top: 15px;
+  }
+}
+</style>
