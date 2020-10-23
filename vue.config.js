46eb8cddec8020c90ec5ba571b201f185231e127
liwang <liwang@segimail.com>
2020-10-23 09:48:50 +0800

    解决大屏时会出现留白问题

--- vue.config.js
+++ vue.config.js
@@ -19,7 +19,7 @@ const getIPAdress = () => {
 
 const localhost = getIPAdress()
 console.log('IP', localhost)
-const target = 'http://192.168.1.11:10080'
+// const target = 'http://192.168.1.11:10080'
 // const target = 'http://192.168.3.28:8080' // 周柏甫
 // const mock = 'http://192.168.3.34:8081'
 // const target = 'http://beta.uhomecp.com/'
@@ -27,7 +27,7 @@ const target = 'http://192.168.1.11:10080'
 // const target = 'http://192.168.3.34:8081'
 // const target = 'http://192.168.3.28:8080'
 // const mock = 'http://192.168.3.34:8081'
-// const target = 'http://beta.uhomecp.com/'
+const target = 'http://beta.uhomecp.com/'
 // const target = `http://${localhost}:8089`
 const proxyURL = [
   '/uhomecp-sso/',
