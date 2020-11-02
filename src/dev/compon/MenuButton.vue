7f863114d9a2099319d85de0068744ab76e5773e
liwang <liwang@segimail.com>
2020-11-02 13:56:42 +0800

    解决报错问题

--- src/dev/compon/MenuButton.vue
+++ src/dev/compon/MenuButton.vue
@@ -49,8 +49,8 @@
         <template slot="title">
           <span>版本需求文档</span>
         </template>
-        <a-button type="solution" @click="Rpd">
-          <a-icon type="snippets" />
+        <a-button type="dashed" @click="Rpd">
+          <a-icon type="solution" />
         </a-button>
       </a-tooltip>
     </a-button-group>
@@ -90,6 +90,9 @@ export default {
     // open lanhu Ui
     openLanhu () {
       window.open('https://lanhuapp.com/web/#/item/project/board?pid=40bdae3a-630a-46bd-a38e-993a6b8ea422&teamId=d22c4265-5e0a-42cc-b87b-c861a865c661')
+    },
+    openUi () {
+      window.open('http://sgui.rxshc.com/segi-ant/index.html#/comlist/sg-button?tdsourcetag=s_pctim_aiomsg')
     },
     Rpd () {
       window.open('https://docs.qq.com/sheet/DQnVUTmFLcG9aS0Fv?tab=bj226v&_t=1604282931509&groupUin=NlD0v%25252FXQQk7tB6rSk%25252BkIpQ%25253D%25253D&ADUIN=916166346&ADSESSION=1604281281&ADTAG=CLIENT.QQ.5627_.0&ADPUBNO=27047')
