50cb3d7d373d28615cbd2948179f59cb00e45584
liwang <liwang@segimail.com>
2020-11-23 11:48:59 +0800

    修改字

--- src/views/assetInformationManagement/assetsTable/index.vue
+++ src/views/assetInformationManagement/assetsTable/index.vue
@@ -101,7 +101,7 @@
                         v-model.trim="queryData.cleaningOrderCode"
                         style="width: 190px"
                         :maxLength="30"
-                        placeholder="出库单名称/编码"
+                        placeholder="出库单名称/编号"
                 />
             </div>
         </SG-SearchContainer>
