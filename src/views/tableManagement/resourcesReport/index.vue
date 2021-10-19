fe22e6800731f7c6df8cc18b887875e3adcff9d4
wzw <515335161@qq.com>
2021-10-19 15:10:22 +0800

    fix(报表): [待联调] 新增 统计显示

--- src/views/tableManagement/resourcesReport/index.vue
+++ src/views/tableManagement/resourcesReport/index.vue
@@ -65,6 +65,10 @@ const columnsData = [
   { title: '房屋数量', dataIndex: 'houseNum', width: 150 },
   { title: '房屋总面积(㎡)', dataIndex: 'houseTotalArea', width: 150 },
   { title: '已租面积(㎡)', dataIndex: 'rentedArea', width: 150 },
+  { title: '租赁面积(㎡)', dataIndex: 'leaseArea', width: 150 },
+  { title: '自用面积(㎡)', dataIndex: 'oneselfArea', width: 150 },
+  { title: '闲置面积(㎡)', dataIndex: 'idleArea', width: 150 },
+  { title: '销售面积(㎡)', dataIndex: 'sellArea', width: 150 },
   { title: '资产原值(元)', dataIndex: 'originalValue', width: 150 },
   { title: '最新估值(元)', dataIndex: 'marketValue', width: 150 },
   { title: '操作', key: 'action', scopedSlots: { customRender: 'action' }, width: 100}
