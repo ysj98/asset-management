0f58fcccaeb2e178a49bcba09a9092d5447f2649
wzw <515335161@qq.com>
2021-11-03 16:57:49 +0800

    feat: 增加 请求接口配置 增加字典编码配置

--- src/store/modules/platformDict.js
+++ src/store/modules/platformDict.js
@@ -1,19 +1,33 @@
 // 字典编码
 import * as apiAsset from "api/assets";
 
-import {SET_DEPRECIATION_METHOD} from '../types/platformDictTypes'
+import {
+  SET_DEPRECIATION_METHOD,
+  SET_AMS_USE_DIRECTION,
+  SET_EQUIPMENT_CODE_SUB
+} from "../types/platformDictTypes";
 const state = {
   // 折旧方法
-  DEPRECIATION_METHOD: []
+  DEPRECIATION_METHOD: [],
+  // 使用方向
+  AMS_USE_DIRECTION: [],
+  // 专业分类
+  EQUIPMENT_CODE_SUB: []
 };
 const mutations = {
   [SET_DEPRECIATION_METHOD](state, payload) {
     state.DEPRECIATION_METHOD = payload;
+  },
+  [SET_AMS_USE_DIRECTION](state, payload) {
+    state.AMS_USE_DIRECTION = payload;
+  },
+  [SET_EQUIPMENT_CODE_SUB](state, payload) {
+    state.EQUIPMENT_CODE_SUB = payload;
   }
 };
 
 const actions = {
-  getPlatformDict({ commit }, {code,type}) {
+  getPlatformDict({ commit }, { code, type }) {
     apiAsset.platformDict({ code: code }).then(res => {
       if (res.data.code === "0") {
         let data = res.data.data;
