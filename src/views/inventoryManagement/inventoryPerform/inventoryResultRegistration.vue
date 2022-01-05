908d2ef958f306099432faeeb8b63e729040af84
wzw <515335161@qq.com>
2022-01-05 18:40:25 +0800

    feat: 盘点执行-录入盘点结果 附件使用新接口并改变大小限制

--- src/views/inventoryManagement/inventoryPerform/inventoryResultRegistration.vue
+++ src/views/inventoryManagement/inventoryPerform/inventoryResultRegistration.vue
@@ -37,7 +37,7 @@
           <a-form :form="form" @submit="handleSubmit">
             <a-col class="playground-col" :span="24">
               <a-form-item v-bind="formItemLayout" label="盘点结果：">
-                <a-radio-group 
+                <a-radio-group
                   v-decorator="['checkResult',
                     {rules: [{required: true}], initialValue: newCardData.checkResult}
                   ]"
@@ -74,7 +74,22 @@
             <a-col class="playground-col" :span="24">
             <a-form-item v-bind="formItemTextarea" label="上传图片：">
               <SG-UploadFile
+                type="all"
                 v-model="newCardData.files"
+                :maxSize="20480"
+                :customDownload="
+                  (value) => {
+                    return customDownload(
+                      value,
+                      $api.ownership.downLoadAnnex
+                    );
+                  }
+                "
+                :customUpload="
+                  (value) => {
+                    return customUpload(value, $api.ownership.uploadAnnex);
+                  }
+                "
               />
             </a-form-item>
           </a-col>
@@ -82,14 +97,16 @@
         </a-row>
     </div>
   </div>
-  </SG-Modal>  
+  </SG-Modal>
   </div>
 </template>
 
 <script>
+import uploadAndDownLoadFIle from "@/mixins/uploadAndDownLoadFIle";
 export default {
   components: {},
   props: {},
+  mixins:[uploadAndDownLoadFIle],
   data () {
     return {
       resultId: '',
@@ -252,16 +269,9 @@ export default {
             } else {
               this.requiredShow = true
             }
-            let files = []
             if (data.attachmentList && data.attachmentList.length > 0) {
-              data.attachmentList.forEach(item => {
-              files.push({
-                url: item.attachmentPath,
-                name: item.oldAttachmentName
-              })
-            })
-          }
-          this.newCardData.files = files
+              this.newCardData.files = data.attachmentList.map(ele=>({url: ele.attachmentPath,name:ele.oldAttachmentName,attachmentId:ele.attachmentId}))
+            }
           }
         } else {
           this.$message.error(res.data.message)
