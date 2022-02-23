10e15bcf27d0707e8eb7dd67a95724a799a1f413
Gitqiaos5 <397145447@qq.com>
2022-02-23 17:04:00 +0800

    修改样式

--- src/views/assetInformationManagement/house-account/building-view/editTag.vue
+++ src/views/assetInformationManagement/house-account/building-view/editTag.vue
@@ -40,9 +40,9 @@
         width: 23%;
         padding: 5px 0;
       }
-      & :nth-child(6) {
-        width: 258px;
-      }
+      // & :nth-child(6) {
+      //   width: 258px;
+      // }
     }
   }
 </style>
